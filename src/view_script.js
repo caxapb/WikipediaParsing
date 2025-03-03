8fb339d51b9e15f662edb4edfaae7c168b1987a7
Anastasia Pichugina <a.pichugina@innopolis.university>
2025-03-03 21:55:12 +0300

    web files are modified

--- src/view_script.js
+++ src/view_script.js
@@ -8,6 +8,9 @@ document.addEventListener('DOMContentLoaded', function () {
     const rowBody = document.getElementById('row-view');
     const sortByYear = document.getElementById('sort-year');
     const sortByRevenue = document.getElementById('sort-revenue');
+    const seeStatsButton = document.getElementById('see-stats');
+    const statsModal = document.getElementById('stats');
+    const closeModal = document.getElementById('close');
  
 
     let films = [];
@@ -15,12 +18,10 @@ document.addEventListener('DOMContentLoaded', function () {
     let currentSort = 'rev';
 
     fetch('./data/films.json')
-        .then(response => response.json())
-        .then(data => {
+        .then(response => response.json()).then(data => {
             films = data;
             displayTableView(films);
-            displayRowView(films);
-        });
+            displayRowView(films);});
 
     function displayTableView(data) {
         tableBody.innerHTML = '';
@@ -29,9 +30,9 @@ document.addEventListener('DOMContentLoaded', function () {
             row.innerHTML = `
                 <td style="padding-left:10px;">${film.title}</td>
                 <td style="text-align: center;">${film.year}</td>
-                <td style="text-align: center;">${film.director.join(', ')}</td>
+                <td style="text-align: center;">${film.director[0]}</td>
                 <td style="text-align: center;">$${film.revenue.toLocaleString()}</td>
-                <td style="padding-left:10px;">${film.country.join(', ')}</td>
+                <td style="padding-left:10px;">${film.country[0]}</td>
             `;
             tableBody.appendChild(row);
         });
@@ -47,9 +48,9 @@ document.addEventListener('DOMContentLoaded', function () {
                 <div class="film-data">
                     <h1>${film.title}</h2>
                     <p><strong>Year:</strong> ${film.year}</p>
-                    <p><strong>Director:</strong> ${film.director.join(', ')}</p>
+                    <p><strong>Director:</strong> ${film.director[0]}</p>
                     <p><strong>Revenue</strong> (millions): $${film.revenue.toLocaleString()}</p>
-                    <p><strong>Country:</strong> ${film.country.join(', ')}</p>
+                    <p><strong>Country:</strong> ${film.country[0]}</p>
                 </div>
             `;
             rowBody.appendChild(row);
@@ -67,6 +68,69 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    function generatePlot(films) {
+        const plot = document.getElementById('plot');
+        plot.innerHTML = '';
+
+        const filmsByYear = {};
+        films.forEach(film => {
+            if (!filmsByYear[film.year]) {
+                filmsByYear[film.year] = [];
+            }
+            filmsByYear[film.year].push(film);
+        });
+
+        const years = Object.keys(filmsByYear).sort((a, b) => a - b);
+        const maxFilms = Math.max(...Object.values(filmsByYear).map(arr => arr.length));
+        let order = -1;
+
+        years.forEach(year => {
+            order = order + 1;
+
+            const filmsInYear = filmsByYear[year];
+            const x = order * 4.2;
+            const y = 100 - (filmsInYear.length / maxFilms) * 90;
+
+
+            const circle = document.createElement('div');
+            circle.className = 'circle';
+            circle.style.left = `${x+5}%`;
+            circle.style.top = `${y}%`;
+
+            const yearX = document.createElement('div');
+            yearX.textContent = year;
+            yearX.className = 'yearX';
+            yearX.style.left = `${x+5}%`; 
+            yearX.style.top = `100%`;
+
+            const filmAmount = document.createElement('div');
+            filmAmount.textContent = '-'+filmsInYear.length;
+            filmAmount.className = 'yearY';
+            filmAmount.style.left = '0%';
+            filmAmount.style.top = `${y}%`;
+
+
+            const tooltip = document.createElement('div');
+            tooltip.className = 'tooltip';
+            tooltip.textContent = filmsInYear.map(film=>film.title).join(', ');
+
+            circle.addEventListener('mouseover', function(){
+                tooltip.style.display = 'block';
+                tooltip.style.left = `${x+2}%`;
+                tooltip.style.top = `${y-10}%`;
+            });
+
+            circle.addEventListener('mouseout', function(){
+                tooltip.style.display = 'none';
+            });
+
+            plot.appendChild(circle);
+            plot.appendChild(yearX);
+            plot.appendChild(filmAmount);
+            plot.appendChild(tooltip);
+        });
+    }
+
     
     switchButton.addEventListener('click', function () {
         isTableView = !isTableView;
@@ -81,11 +145,11 @@ document.addEventListener('DOMContentLoaded', function () {
         } else {
             switchButton.textContent = 'Switch to Table View';
             const sorted = films.slice().sort((a, b) => b.revenue - a.revenue);
-            displayTableView(sorted)
+            displayTableView(sorted);
             tableView.classList.add('hidden');
             rowView.classList.remove('hidden');
-            currentSort = 'rev'
-            updateArrow()
+            currentSort = 'rev';
+            updateArrow();
         }
     });
 
@@ -95,7 +159,7 @@ document.addEventListener('DOMContentLoaded', function () {
         if (isTableView) {
             displayTableView(foundFilms);
         } else {
-            displayRowView(foundFilms)
+            displayRowView(foundFilms);
         }
     });
     
@@ -103,24 +167,35 @@ document.addEventListener('DOMContentLoaded', function () {
         if (currentSort === 'year') return;
         const sorted = films.slice().sort((a, b) => a.year - b.year);
         if (isTableView) {
-            displayTableView(sorted)
+            displayTableView(sorted);
         } else {
-            displayRowView(sorted)
+            displayRowView(sorted);
         }
-        currentSort = 'year'
-        updateArrow()
+        currentSort = 'year';
+        updateArrow();
     });
 
     sortByRevenue.addEventListener('click', function () {
         if (currentSort === 'rev') return;
         const sorted = films.slice().sort((a, b) => b.revenue - a.revenue);
         if (isTableView) {
-            displayTableView(sorted)
+            displayTableView(sorted);
         } else {
-            displayRowView(sorted)
+            displayRowView(sorted);
         }
-        currentSort = 'rev'
-        updateArrow()
+        currentSort = 'rev';
+        updateArrow();
+    });
+
+    seeStatsButton.addEventListener('click', function () {
+        statsModal.classList.remove('hidden');
+        statsModal.style.display = 'block';
+        generatePlot(films);
+    });
+
+    closeModal.addEventListener('click', function () {
+        statsModal.classList.add('hidden');
+        statsModal.style.display = 'none';
     });
 
     updateArrow();
